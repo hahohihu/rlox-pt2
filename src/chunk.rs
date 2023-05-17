d72abfddbb18c91869e043f1c6006fd841963c4d
hahohihu <pshen4@gmail.com>
2023-05-17 12:02:45 -0700

    some basic chunk stuff

--- src/chunk.rs
+++ src/chunk.rs
@@ -1,32 +1,44 @@
 use num_enum::FromPrimitive;
 
+use crate::value::Value;
+
 #[derive(Debug, Eq, PartialEq, FromPrimitive)]
 #[repr(u8)]
 pub enum OpCode {
-    Return,
+    Return, // 0 follow bytes
+    Constant, // 1 follow byte for a constant index
     #[num_enum(default)]
     Invalid,
 }
 
 #[derive(Default, Debug, Clone)]
-pub struct Chunk(Vec<u8>);
+pub struct Chunk {
+    // INVARIANT: An OpCode must be followed by however many bytes are specified
+    instructions: Vec<u8>,
+    constants: Vec<Value>,
+}
 
 impl Chunk {
     pub fn new() -> Self {
         Self::default()
     }
+
+    pub fn add_constant(&mut self, value: Value) -> usize {
+        self.constants.push(value);
+        self.constants.len() - 1
+    }
     
     pub fn disassemble(&self, name: &str) {
         println!("==== {name} ====");
         let mut i = 0;
-        while i < self.0.len() {
+        while i < self.instructions.len() {
             self.disassemble_instruction(&mut i);
         }
     }
 
     /// SAFETY: OpCode invariants must be upheld. If an opcode is n bytes, n bytes _must_ be inserted
     pub unsafe fn write_byte(&mut self, byte: u8) {
-        self.0.push(byte)
+        self.instructions.push(byte)
     }
 
     fn simple_instruction(name: &str, offset: &mut usize) {
@@ -34,13 +46,21 @@ impl Chunk {
         *offset += 1;
     }
 
+    fn constant_instruction(&self, name: &str, offset: &mut usize) {
+        let index = self.instructions[*offset + 1];
+        let value = self.constants[index as usize];
+        println!("{name:<16} {index:>4} '{value}'");
+        *offset += 2;
+    }
+
     fn disassemble_instruction(&self, offset: &mut usize) {
         print!("{:0>4} ", *offset);
 
-        let chunk = self.0[*offset];
+        let chunk = self.instructions[*offset];
         let instruction: OpCode = chunk.into();
         match instruction {
-            OpCode::Return => Self::simple_instruction("RETURN", offset),
+            OpCode::Return => Chunk::simple_instruction("RETURN", offset),
+            OpCode::Constant => self.constant_instruction("CONSTANT", offset),
             OpCode::Invalid => {
                 println!("INVALID OPCODE: {chunk}");
                 *offset += 1;
--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,15 @@ mod lex;
 mod parse;
 mod ui;
 mod chunk;
+mod value;
 
 fn main() {
     let mut chunk = Chunk::new();
     unsafe {
         chunk.write_byte(OpCode::Return as u8);
+        chunk.add_constant(42.0);
+        chunk.write_byte(OpCode::Constant as u8);
+        chunk.write_byte(0);
     }
     chunk.disassemble("test");
 }
